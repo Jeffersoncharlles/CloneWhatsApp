693649245e4cddedd71c359fa695ebbb2d1093b2
jeffersoncharlles <contato@jefferdeveloper.com>
2022-04-25 19:53:38 -0300

    chore:transition commit

--- src/components/ChatWindow/index.tsx
+++ src/components/ChatWindow/index.tsx
@@ -1,3 +1,4 @@
+import EmojiPicker, { IEmojiData } from 'emoji-picker-react';
 import styles from './styles.module.scss'
 import SearchIcon from '@mui/icons-material/Search';
 import MoreVertIcon from '@mui/icons-material/MoreVert';
@@ -7,8 +8,53 @@ import CloseIcon from '@mui/icons-material/Close';
 import SendIcon from '@mui/icons-material/Send';
 import MicIcon from '@mui/icons-material/Mic';
 import SettingsVoiceIcon from '@mui/icons-material/SettingsVoice';
+import { useState } from 'react';
+
+declare const window: any;
 
 export const ChatWindow = () => {
+    const [openEmoji, setOpenEmoji] = useState(false)
+    const [message, setMessage] = useState('')
+    const [listening, setListening] = useState(false)
+
+    const handleEmojiClick = (event: React.MouseEvent, data: IEmojiData) => {
+        //receber o emoji clicado
+        setMessage(message + data.emoji)
+    }
+
+    const handleSendClick = () => {
+
+    }
+
+    let recognition: any = null;
+    let SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+    if (SpeechRecognition !== undefined) {
+        recognition = new SpeechRecognition();
+        recognition.lang = 'pt-BR';
+    }
+
+
+    const handleMicClick = () => {
+        if (recognition !== null) {
+            recognition.onstart = () => {
+                setListening(true);
+                //escutar mic
+            }
+            recognition.onend = () => {
+                setListening(false);
+                //parar mic
+            }
+            recognition.onresult = (e: any) => {
+                setMessage(e.results[0][0].transcript)
+                //resultado transcrição
+            }
+            recognition.start()
+        }
+    }
+
+
+
+
 
     return (
         <section className={styles.container}>
@@ -34,25 +80,59 @@ export const ChatWindow = () => {
             <article>
 
             </article>
+            <div className={styles.content_E} style={{ height: openEmoji ? '350px' : '0' }}>
+                <EmojiPicker
+                    disableSearchBar
+                    disableSkinTonePicker
+                    onEmojiClick={handleEmojiClick}
+                />
+            </div>
             <footer>
                 <div className={styles.pre}>
-                    <div className={styles.btn_c}>
-                        <InsertEmoticonIcon />
+                    <div className={styles.btn_c}
+                        onClick={() => setOpenEmoji(false)}
+                    >
+                        {openEmoji && (
+                            <CloseIcon />
+                        )}
+
+                    </div>
+                    <div className={styles.btn_c}
+                        onClick={() => setOpenEmoji(true)}
+                    >
+                        {!openEmoji && (
+                            <InsertEmoticonIcon />
+                        )}
+
                     </div>
                 </div>
 
                 <div className={styles.content}>
                     <input type="text"
                         placeholder='Digite uma mensagem'
-
+                        value={message}
+                        onChange={(e) => setMessage(e.target.value)}
 
                     />
                 </div>
 
                 <div className={styles.send}>
-                    <div className={styles.btn_c}>
-                        <SendIcon />
-                    </div>
+                    {message === '' ? (
+                        <div className={styles.btn_c}
+                            onClick={handleMicClick}
+                        >
+                            {listening ? (<SettingsVoiceIcon className={styles.btn_c_b} />)
+                                : (<MicIcon />)}
+                        </div>
+                    ) : (
+                        <div className={styles.btn_c}
+                            onClick={handleSendClick}
+                        >
+                            <SendIcon />
+                        </div>
+                    )}
+
+
                 </div>
 
             </footer>
