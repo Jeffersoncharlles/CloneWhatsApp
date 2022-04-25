d5e268c492a435c982f8ea8435bff5c5f55700aa
jeffersoncharlles <contato@jefferdeveloper.com>
2022-04-25 10:43:22 -0300

    chore:start layout nav commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,54 @@
+import { useEffect, useState } from 'react';
+
+import styles from './style/app.module.scss'
+import DonutLargeIcon from '@mui/icons-material/DonutLarge';
+import ChatIcon from '@mui/icons-material/Chat';
+import MoreVertIcon from '@mui/icons-material/MoreVert';
+import SearchIcon from '@mui/icons-material/Search';
+import { ChatListItem } from './components/ChatListItem';
+
 
 function App() {
+  const [chatList, setChatList] = useState([{}, {}, {}, {}])
 
+  useEffect(() => {
+
+  }, [])
 
   return (
-    <div className="App">
+    <div className={styles.app}>
+      <nav className={styles.container_Sidebar}>
+        <header>
+          <img src="https://randomuser.me/api/portraits/men/75.jpg" alt="" />
+          <div>
+            <div className={styles.btn}>
+              <DonutLargeIcon />
+            </div>
+            <div className={styles.btn}>
+              <ChatIcon />
+            </div>
+            <div className={styles.btn}>
+              <MoreVertIcon />
+            </div>
+          </div>
+        </header>
 
+        <div className={styles.container_Sidebar_Search}>
+          <div>
+            <SearchIcon fontSize='small' style={{ color: '#919191' }} />
+            <input type="search" placeholder='Procurar ou começar uma nova conversa' />
+          </div>
+        </div>
+        <div className={styles.container_Sidebar_Chart}>
+          {chatList.map((item, index) => (
+            <ChatListItem key={index} data={item} />
+          ))}
+        </div>
+      </nav>
+
+      <main className={styles.Container_Main}>
+        ...
+      </main>
     </div>
   )
 }
---
+++ src/components/ChatListItem/index.tsx
@@ -0,0 +1,16 @@
+import styles from './styles.module.scss'
+
+interface IChat {
+    data: {
+
+    }
+}
+
+export const ChatListItem = ({ }: IChat) => {
+
+    return (
+        <section>
+
+        </section>
+    );
+}
